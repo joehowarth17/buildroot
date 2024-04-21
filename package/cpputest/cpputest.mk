5105b5b8cf925dc6efefe0589a08296aa4a61a61
Joseph <josephhowarth17@gmail.com>
2024-04-21 00:03:34 -0400

    Update cpputest to install in staging dir.
    -Since it is library needed for development, we need to enable install in staging dir, not on target.
    -Maybe we do want for target, to run tests on the hardware, but for now disable since we only use buildroot for cross compile.

--- package/cpputest/cpputest.mk
+++ package/cpputest/cpputest.mk
@@ -6,7 +6,8 @@ CPPUTEST_VERSION = 4.0
 CPPUTEST_SOURCE = cpputest-$(CPPUTEST_VERSION).tar.gz 
 CPPUTEST_SITE = https://github.com/cpputest/cpputest/releases/download/v$(CPPUTEST_VERSION)
 
-
+CPPUTEST_INSTALL_STAGING=YES
+CPPUTEST_INSTALL_TARGET=NO
 CPPUTEST_CONF_OPTS = -DTESTS=OFF
 $(eval $(cmake-package))
 
